25f2c10b14bb5c56e06463fcb0fecec474177bf6
Ahmedelsayed55 <am1096922@gmail.com>
2025-10-18 22:45:23 +0300

    updateToHero

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import heroImg from "C:/Users/Wael/Desktop/Tasks/reactTask/src/assets/Hero Image.png";
+import heroImg from "../assets/HeroImage.png";
+
+
+
 export const Hero = () => {
   return (
-    <div className="container relative ">
-      <img src={heroImg} alt="Hero Photo" className="w-[100%] max-w-[1002px]" />
-      <div className="w-[1160px] absolute top-1/2 left-1/7 flex ">
-        <div className="w-1/2">
-          <h1>Body Mass Index Calculator</h1>
-          <p className="w-1/2">
+    <div className=" bg-[url('../assets/HeroImage.png')] relative ">
+      <img
+        src={heroImg}
+        alt="Hero Photo"
+        className="w-[100%] max-w-[1202px] h-auto lg:h-[737px]"
+      />
+      <div className="container w-[1260px] absolute left-[14%] top-[25%] flex gap-28">
+        <div className="w-1/2 flex flex-col gap-10">
+          <h1 className="text-7xl font-semibold ">
+            Body Mass Index Calculator
+          </h1>
+          <p className=" text-[1.3rem] text-gray-600">
             Better understand your weight in relation to your height using our
             body mass index (BM) calculator. While BMI is not the sole
             determinant of a healthy weight, it offers a valuable starting point
@@ -15,37 +24,53 @@ export const Hero = () => {
           </p>
         </div>
 
-        <div className="w-1/2 bg-amber-200">
-          <h2>Enter your details below</h2>
+        <div className="w-1/2 bg-white rounded-3xl  shadow-[0_4px_20px_rgba(0,0,0,0.25)] flex flex-col gap-7 p-10">
+          <h2 className="text-2xl font-semibold">Enter your details below</h2>
 
-          <div className="flex justify-btween ">
-
-            <div className="flex gap-1 w-1/2">
-              <input type="radio" name="d" id="one" checked  className=""/>
+          <div className="flex justify-between ">
+            <div className="flex gap-3 w-1/2">
+              <input
+                type="radio"
+                name="d"
+                id="one"
+                defaultChecked
+                className="w-5"
+              />
               <label htmlFor="one">Metric</label>
             </div>
 
-            <div className="flex gap-1 w-1/2">
-              <input type="radio" id="x" name="d" className=""/>
+            <div className="flex gap-3 w-1/2">
+              <input type="radio" id="x" name="d" className="w-5" />
               <label htmlFor="x">Imperial</label>
             </div>
-
           </div>
-          <div className="flex justify-between">
-            <div className="flex justify-between w-1/3 border-2 border-gray-600">
+
+          <div className="flex justify-between gap-5">
+            <div className="flex justify-between w-1/2 border-2 p-3 rounded-lg border-gray-600">
               <p>Hieght</p>
-              <div className="flex">
+              <div className="flex gap-1">
                 <h3>185</h3>
                 <h3 className="text-[#345FF6]">cm</h3>
               </div>
             </div>
-            <div className="flex justify-between w-1/3 border-2 border-gray-600">
+            <div className="flex justify-between w-1/2 border-2 p-3 rounded-lg border-gray-600">
               <p>Hieght</p>
-              <div className="flex ">
+              <div className="flex gap-1">
                 <h3>80</h3>
                 <h3 className="text-[#345FF6]">kg</h3>
               </div>
             </div>
+          </div>
+
+          <div className="flex gap-20 bg-[#345FF6] px-9 py-6 rounded rounded-r-[999px] ">
+            <div className="flex flex-col gap-3 text-white">
+              <p>Your BMI is...</p>
+              <h2 className="text-6xl font-semibold">23.4</h2>
+            </div>
+            <p className="text-white w-1/2">
+              Your BMI suggests you’re a healthy weight. Your ideal weight is
+              between <span className="font-semibold">63.5kgs</span> - <span className="font-semibold">85.2kgs</span>.
+            </p>
           </div>
         </div>
       </div>
