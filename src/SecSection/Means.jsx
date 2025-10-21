77dc188a1529967bcf79496b0ef6b80de082f577
Ahmedelsayed55 <am1096922@gmail.com>
2025-10-21 18:55:06 +0300

    fs

--- src/SecSection/Means.jsx
+++ src/SecSection/Means.jsx
@@ -6,8 +6,8 @@ export const Means = () => {
       <div className="w-[100%] max-w-[1284px] mx-auto flex flex-col lg:flex-row items-center  ">
         <img src={persson} alt="Person" className="block w-1/1 lg:w-1/2" />
         <div className="flex flex-col gap-5 mt-15 w-1/1 lg:w-1/2 ps-15 lg:ps-27" >
-          <h1 className="text-[48px] font-semibold tracking-tight">What your BMI result means</h1>
-          <p className="w-[90%]">
+          <h1 className=" text-4xl lg:text-[48px] font-semibold tracking-tight">What your BMI result means</h1>
+          <p className="w-[100%]">
             A BMI range of 18.5 to 24.9 is considered a 'healthy weight.'
             Maintaining a healthy weight may lower your chances of experiencing
             health issues later on, such as obesity and type 2 diabetes. Aim for
